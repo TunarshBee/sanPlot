1e6b4885d6e085c27e9b12f0bcd736e01c30abf3
TunarshBee <rafiubello4real@gmail.com>
2023-07-17 17:38:06 +0100

    Added readme file to the project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Code Explanation
+
+The provided code defines several classes for creating different types of plots using Matplotlib library in Python. The classes include:
+
+1. **Line**: This class creates a line plot.
+2. **Bar**: This class creates a bar chart.
+3. **Scatter**: This class creates a scatter plot.
+4. **Pie**: This class creates a pie chart.
+5. **Doughnut**: This class creates a doughnut chart.
--- sanPlot/bar.py
+++ sanPlot/bar.py
@@ -1,14 +1,65 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
-plt.figure('seaborn')
+print(plt.style.available)
 
-dev_x = [1,2,3,4,5,6,7,8,9,10,11]
-dev_y = [100,200,300,400,500,600,700,800,900,1000,11000]
+# class Bar:
+#     def __init__(
+#         self,
+#         x_values,
+#         y_values,
+#         title: str = None,
+#         x_ttl: str = None,
+#         y_ttl: str = None,
+#         label: str = None,
+#         graphStyle: str = None,
+#         cmcStyle: bool = None,
+#         color: str = None,
+#         save: str = None,
+#         alpha: int = None,
+#         width: int = None,
+#         idxwidth: int = None,
+#     ):
+#         self.x_values = x_values
+#         self.y_values = y_values
+#         self.title = title
+#         self.x_ttl = x_ttl
+#         self.y_ttl = y_ttl
+#         self.label = label
+#         self.style = graphStyle
+#         self.cmcStyle = cmcStyle
+#         self.color = color
+#         self.save = save
+#         self.width = width
+#         self.alpha = alpha
+#         self.idxwidth = idxwidth
 
-plt.bar(dev_x, dev_y)
+#     def render(self):
+#         plt.style.use(self.style) if self.style else plt.style.use("seaborn")
 
-plt.xlabel("this is the x coordinate")
-plt.ylabel("this is the y coordinate")
-
-plt.title("line graph")
-plt.show()
+#         x_indexes = np.arange(len(self.x_values))
+#         plt.bar(
+#             x_indexes,
+#             self.y_values,
+#             color=self.color if self.color else None,
+#             alpha=self.alpha if self.alpha else None,
+#             label=self.label if self.label else None,
+#         )
+#         # if plt is more than one,
+#         # you must specify idxwidth if you are plotting multiple bar charts at once
+#         plt.bar(
+#             x_indexes(self.idxwidth),
+#             self.y_values,
+#             width=self.width,
+#             color=self.color,
+#             alpha=self.alpha,
+#             label=self.label,
+#         ) if self.idxwidth else " "
+#         plt.xticks(ticks=x_indexes, labels=self.x_values)
+#         plt.xlabel(self.x_ttl) if self.x_ttl else ""
+#         plt.ylabel(self.y_ttl) if self.y_ttl else ""
+#         plt.title(self.title) if self.title else ""
+#         plt.legend(loc="upper left") if self.label else ""
+#         plt.savefig(self.save) if self.save else ""
+#         plt.tight_layout()
+#         plt.show()
