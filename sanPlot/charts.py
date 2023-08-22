38be4ce8bb3894f193b2b050e01eb3e6a3eecd09
TunarshBee <tunarsh4real@gmail.com>
2023-08-22 18:11:37 +0100

    updated the chart module

--- sanPlot/charts.py
+++ sanPlot/charts.py
@@ -1,32 +1,9 @@
 import matplotlib.pyplot as plt
-
+from .pie import Pie
 from .bar import Bar
-from .networkViz import NetworkChart
+# from .networkViz import NetworkChart
 from .line import Line
-from .circle_packing_chart import Circle_Packing
+# from .circle_packing_chart import Circle_Packing
 from .scatter import Scatter
 
-class Pie:
-    def __init__(self, labels, sizes, title):
-        self.labels = labels
-        self.sizes = sizes
-        self.title = title
 
-    def render(self):
-        plt.pie(self.sizes, labels=self.labels)
-        plt.title(self.title)
-        plt.show()
-
-
-class Doughnut:
-    def __init__(self, labels, sizes, title):
-        self.labels = labels
-        self.sizes = sizes
-        self.title = title
-
-    def render(self):
-        fig, ax = plt.subplots()
-        ax.pie(self.sizes, labels=self.labels)
-        ax.axis("equal")  # Equal aspect ratio ensures a circular pie
-        ax.set_title(self.title)
-        plt.show()
